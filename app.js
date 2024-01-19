adfea69ec535290f8b5ee3b843d753be0b630e17
Marcos Ariel Rodrigues de Jesus <marcosariel.rj@hotmail.com>
2024-01-19 12:00:58 -0300

    Adiçao de código

---
+++ app.js
@@ -0,0 +1,62 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+exibirMensagemInicial();
+
+function exibirTextoNaTela(tag, texto){
+
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial(){
+
+    exibirTextoNaTela('h1', 'Jogo do Numero secreto');
+    exibirTextoNaTela('p', 'Digite um numero de 1 a 10');
+    
+}
+
+function verificarChute(){
+
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou');
+        let gramatica = tentativas > 1? "tentativas" : 'tentativa';
+        let mensagemTentativas = `Vc descobriu com ${tentativas} ${gramatica}`
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    }else {
+
+        if(chute > numeroSecreto){
+
+            exibirTextoNaTela('p', 'O num Secreto é menor');
+        }else{
+
+            exibirTextoNaTela('p', 'O num Secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+
+    return parseInt(Math.random() * 10 + 1);
+    
+}
+function limparCampo(){
+
+    document.querySelector('input').value = "";
+};
+
+function reiniciarJogo() {
+    
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+
+}
